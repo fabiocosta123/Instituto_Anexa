b5457e10ba927b8ebd9405d70e0f693169b41205
developerfabiocsilva@outlook.com <developerfabiocsilva@outlook.com>
2025-08-26 17:58:17 -0300

    inserindo verificação de campos vazios e biblioteca toast

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,28 +1,49 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import "@/style/loginForm.css"; // importa o CSS
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@/style/loginForm.css";
 
 export default function LoginForm() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-  const [erro, setErro] = useState("");
 
   const handleLogin = async () => {
+    if (!email && !senha) {
+      toast.error("Campo usuário/senha estão vazios");
+      return;
+    }
+
+    if (!email) {
+      toast.error("Campo usuário não pode ser vazio");
+      return;
+    }
+
+    if (!email.includes("@")) {
+      toast.error("Digite um e-mail válido");
+      return;
+    }
+
+    if (!senha) {
+      toast.error("Campo senha não pode ser vazio");
+      return;
+    }
+
     try {
       const response = await axios.post<{ access_token: string }>(
         "http://localhost:5000/api/login",
         { email, senha }
       );
+
       const { access_token } = response.data;
       localStorage.setItem("token", access_token);
-      setErro("");
       router.push("/dashboard");
     } catch (err) {
-      setErro("Email ou senha inválidos");
+      toast.error("Email ou senha inválidos");
     }
   };
 
@@ -43,10 +64,12 @@ export default function LoginForm() {
         placeholder="Senha"
         className="login-input"
       />
-      {erro && <p className="login-error">{erro}</p>}
       <button onClick={handleLogin} className="login-button">
         Entrar
       </button>
+
+      {/* ✅ Toast container para exibir mensagens */}
+      <ToastContainer position="top-right" autoClose={3000} />
     </div>
   );
 }

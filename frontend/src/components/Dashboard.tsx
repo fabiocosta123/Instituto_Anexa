bf151c8b76e0168455ad2f043cebe363627ae4f1
developerfabiocsilva@outlook.com <developerfabiocsilva@outlook.com>
2025-08-26 18:01:32 -0300

    mudando tamanho do font size do placeholder input

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,18 +1,49 @@
-// src/pages/dashboard.tsx
-import Sidebar from './Sidebar'
-import Header from './Header'
-import '../styles/dashboard.css'
+'use client';
+
+import { useEffect, useState } from "react";
+import api from "../app/services/api";
+
+interface Curso {
+  id: number;
+  titulo: string;
+  descricao: string;
+}
+
+
+//verificação
+useEffect(() => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    router.push("/login");
+  }
+}, []);
 
 export default function Dashboard() {
+  const [cursos, setCursos] = useState<Curso[]>([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    api.get<Curso[]>("/Cursos", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => setCursos(res.data))
+    .catch((err) => console.error("Erro ao buscar cursos", err));
+  }, []);
+
   return (
-    <div className="dashboard-container">
-      <Sidebar />
-      <div className="main-content">
-        <Header />
-        <main className="dashboard-main">
-          <h2>Dashboard em construção...</h2>
-        </main>
-      </div>
+    <div>
+      <h2>Meus Cursos</h2>
+      <ul>
+        {cursos.map((curso) => (
+          <li key={curso.id}>
+            <strong>{curso.titulo}</strong><br />
+            <span>{curso.descricao}</span>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
